078b00d477714948048dc8d97b7353443a62a7bf
Bogdan <bogdan.oloeriu@gmail.com>
2015-04-12 22:39:49 +0200

    updated gitignore

--- app/src/main/java/eu/mysmartline/appv3/Activity3ListActivityes.java
+++ app/src/main/java/eu/mysmartline/appv3/Activity3ListActivityes.java
@@ -31,6 +31,10 @@ import com.google.gson.Gson;
 
 import eu.mysmartline.appv3.models.MyKeys;
 
+/**
+ * TODO Refactor list show it shows also icons.
+ * This is the main screen.
+ */
 public class Activity3ListActivityes extends Activity {
 	private List<Map<String, String>> items = new ArrayList<Map<String, String>>();
 	private String logTag = MyKeys.PROPERTY_LOGTAG;
